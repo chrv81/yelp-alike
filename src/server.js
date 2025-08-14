4f6f1d43440cedd8e4f8a03a61a790d73163fcfc
chrv81 <vochris81@gmail.com>
2025-08-13 21:38:00 -0400

    Add error handling middleware and integrate it into the server

--- src/server.js
+++ src/server.js
@@ -1,13 +1,45 @@
+/* eslint-disable no-unused-vars */
 import express from 'express';
 import cors from 'cors';
 
+import rootRouter from './routers/root.router';
+
+import { responseError } from './common/utilities/response.utility';
+import { appError } from './common/app-error/app-error.error';
+
 const app = express();
+// express.json(); // Middleware to parse JSON bodies
 
-app.use(express.json());
+// giúp body nhận dc dữ liệu từ client gửi lên
+app.use(express.json()); // Placeholder for middleware
 
-app.use(cors({
-  origin: ['http://localhost:3000']
-}));
+// luôn để cors trước router api
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+  })
+);
 
+// router api
+app.use('/api', rootRouter);
 
+// xử lý lỗi chung cho toàn bộ ứng dụng
+app.use(appError);
 
+app.use((err, req, res, next) => {
+  console.error('Error in middleware:', err);
+  // res.status(500).json({ error: 'Internal Server Error' });
+  const resData = responseError(err, err?.message, err?.code, err?.stack);
+
+  res.status(resData.statusCode).json(resData);
+});
+
+const port = 3069;
+/**
+ * Starts the server and listens on the specified port.
+ * @param {number} port - The port number to listen on.
+ * @param {Function} callback - Optional callback function to execute when the server starts.
+ */
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
